f12f8bf350f8899b428d999d9122a1baa432fcee
Manish Behl <manish.behl@mac34s.local>
2019-04-04 18:28:43 +0530

    Initial Commit

---
+++ TheMoviesRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesRating
+//
+//  Created by Manish Behl on 04/04/19.
+//  Copyright © 2019 Manish Behl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
